61bd41aab6d28e13ecd087ba194a06cf3a3b13f0
Your Name <you@example.com>
2023-04-05 18:02:03 -0300

    Initial commit

---
+++ src/main/java/com/spring/plenojavainterview/PlenoJavaInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.spring.plenojavainterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlenoJavaInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlenoJavaInterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/plenojavainterview/PlenoJavaInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.plenojavainterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlenoJavaInterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
